c1543d0241d252d8953a583dba40bfc6772e4717
aticina <115451545+aticina@users.noreply.github.com>
2022-11-13 18:18:36 +0100

    novije
    Update prethodnog

--- baza.c
+++ baza.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define EXIT_SUCCESS 0
 #define EXIT_ERROR -1
 #define C_MAX 1000
@@ -15,7 +16,7 @@ int Push(Position P, char [C_MAX]);
 int Add(Position P);
 int Minus(Position P);
 int Multiply(Position P);
-int ReadStog(); /*pročitat će konačni rezultat tj. jednin preostali element stoga te testirati je li sve ok*/
+int Ispis(Position P); /*pročitat će konačni rezultat tj. jednin preostali element stoga te testirati je li sve ok*/
 
 int main(){
 
@@ -32,8 +33,8 @@ int main(){
     }
     
     char string[C_MAX] = {0};
-    if(fscanf(pok, "%s", string)){
-
+    if(fscanf(pok, "\n %[^\n]", string)){
+        printf("Ovo je string %s",string); /* Success */
     }
     else{
         printf("Greška sa fscanf!");
@@ -42,19 +43,23 @@ int main(){
     if(string == NULL) printf("Greška pri čitanju iz dadoteke!");
 
     char provjera[C_MAX] = {0}; /*mjesto u memoriji gdje je iščitani podatak iz dadotke privremeno spremljen kako bi mogli odrediti što s tim podatkom treba dalje raditi-koju funkciju zvati*/
-    sscanf(string, "%s", provjera);
-    
-    if(strcmp(provjera, "+") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0") ){ /*drugi uvjet osigurava da operator ne dolazi na prazan stog, treći da imamo 2 operanda, četvrti da prvi operand nije nula*/
-        Add(&head);
-    }
-    else if(strcmp(provjera, "-") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0")){
-        Minus(&head);
-    }
-    else if(strcmp(provjera, "*") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0")){
-        Multiply(&head);
-    }
-    else if(strcmp(provjera,"\t") == 0){ /*uvjet da ignorira razmake?*/
-        Push(&head, provjera);
+ 
+    while(sscanf(string, "%s", provjera)){
+
+        printf("\n Ovo je provjera %s", provjera); /*success*/
+
+        if(strcmp(provjera, "+") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0") ){ /*drugi uvjet osigurava da operator ne dolazi na prazan stog, treći da imamo 2 operanda, četvrti da prvi operand nije nula*/
+            Add(head);
+        }
+        else if(strcmp(provjera, "-") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0")){
+            Minus(head);
+        }
+        else if(strcmp(provjera, "*") == 0 && head->next != NULL && head->next->next != NULL && strcmp(head->next->uneseno,"0")){
+            Multiply(head);
+        }
+        else /*if(strcmp(provjera,"\t") == 0)*/{ /*uvjet da ignorira razmake?*/
+            Push(head, provjera);
+        }
     }
     
 
@@ -74,6 +79,7 @@ int Push(Position P, char provjera[C_MAX]){
     
 
     printf("\n %s", temp->uneseno);
+    Ispis(P->next);
 
     return EXIT_SUCCESS;
 }
@@ -87,9 +93,11 @@ int Add(Position P){
     D = P->next->next;
     L = P->next;
 
-    strcpy(D->uneseno,(char)((int)D->uneseno + (int)L->uneseno));
+    strcpy(D->uneseno, D->uneseno + (int)L->uneseno);
     P->next = D;
     free(L);
+
+    Ispis(P->next);
 
     return EXIT_SUCCESS;
 }
@@ -103,9 +111,11 @@ int Minus(Position P){
     D = P->next->next;
     L = P->next;
 
-    strcpy(D->uneseno,(char)((int)D->uneseno - (int)L->uneseno));
+    strcpy(D->uneseno, D->uneseno - (int)L->uneseno);
     P->next = D;
     free(L);
+
+    Ispis(P->next);
 
     return EXIT_SUCCESS;
 }
@@ -119,9 +129,30 @@ int Multiply(Position P){
     D = P->next->next;
     L = P->next;
 
-    strcpy(D->uneseno,(char)((int)D->uneseno * (int)L->uneseno));
+    /*strcpy(D->uneseno, D->uneseno × (int)L->uneseno); problem s množenjem*/
     P->next = D;
     free(L);
 
     return EXIT_SUCCESS;
+}
+
+int Ispis(Position P)
+{
+
+    if(P == NULL){
+        printf("\nOva lista je prazna\n");
+		return EXIT_ERROR;
+	}
+    else
+    {
+        printf("\nIspis liste: \n");
+        while (P!=NULL)
+        {
+            printf("%s", P->uneseno );
+            P=P->next;
+        }
+        printf("\n");
+
+		return EXIT_SUCCESS;
+    }
 }
